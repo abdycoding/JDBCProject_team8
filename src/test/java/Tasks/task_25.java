0addaf3c97ab8e924c1272d1dcfd41a3d53e92c8
Abylmyrat Gummanov <abdy.coding@gmail.com>
2024-11-13 15:09:08 +0300

    task 25 is done

--- src/test/java/Tasks/task_25.java
+++ src/test/java/Tasks/task_25.java
@@ -5,23 +5,27 @@ import Utilities.DBUtility;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class task_24 extends DBUtility {
+public class task_25 extends DBUtility {
     public static void main(String[] args) throws SQLException {
         DBConnectionOpen();
 
-        String sql = "  select * from employees\n" +
-                " where employees.hire_date  between '1990-02-20' and '1995-02-20';";
+        String sql = "  select *  \n" +
+                " from employees\n" +
+                " where employees.first_name = 'Annemarie' and employees.last_name='Redmiles';";
 
         ResultSet resultSet = statement.executeQuery(sql);
 
         while (resultSet.next()) {
             // Example of processing the data, adjust as needed based on column names
-
+            int empNo= resultSet.getInt("emp_no");
             String first_name = resultSet.getString("first_name");
             String last_name = resultSet.getString("last_name");
+            String gender = resultSet.getString("gender");
+            String dateofBirth = resultSet.getString("birth_date");
             String hire_date = resultSet.getString("hire_date");
 
-            System.out.println(" first_name: " + first_name +", last_name: " + last_name + ", hire_date: " + hire_date);
+            System.out.println("emp_no: " + empNo + ", first_name: " + first_name +", last_name: " + last_name + ", gender: " + gender+
+                    ", dateofBirth: " + dateofBirth+", hire_date: " + hire_date);
         }
 
         DBConnectionCLose();
